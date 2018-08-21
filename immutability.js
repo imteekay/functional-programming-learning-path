91ae903675562c63531e0b262118e94a7b5d5d6b
TK <leandrotk100@gmail.com>
2018-08-20 23:20:29 -0300

    Immutability

---
+++ immutability.js
@@ -0,0 +1,6 @@
+/* Strings are immutable */
+var name = 'TK';
+name[0] = 'D';
+console.log(name); // 'TK'
+
+/* immutable data is important because we need to be able to attach dependable, unchanging values to our functions */
