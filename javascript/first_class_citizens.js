e544c9307ebb1952de6191a36ee77c3dc3fe845c
TK <leandrotk100@gmail.com>
2018-08-22 23:21:26 -0300

    Learning functions as first class citizens

---
+++ javascript/first_class_citizens.js
@@ -0,0 +1,6 @@
+/*
+  Functions as first class citizens: Treats functions as values
+    - Refer to it from constants and variables
+    - Pass it as a parameter to other functions
+    - Return it as result from other functions
+*/
