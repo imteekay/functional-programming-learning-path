986650389305a07343c0f6cdd3796e200063def7
TK <leandrotk100@gmail.com>
2018-10-09 22:10:36 -0300

    Hacker Rank: solve filter array problem

---
+++ programming_challenges/hacker_rank/filter-array.clj
@@ -0,0 +1,8 @@
+(defn filter-array
+  [n list]
+  (if (< (first list) n)
+    (println (first list)))
+  (if (not (empty? (rest list)))
+    (filter-array n (rest list))))
+
+(filter-array 25 [-41 46 -28 21 52 83 -29 84 27 40])
