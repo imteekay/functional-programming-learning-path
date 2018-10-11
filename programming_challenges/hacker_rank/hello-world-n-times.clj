0bf9eb346c84cd23ef55b26ea8160a1342828ac2
TK <leandrotk100@gmail.com>
2018-10-10 22:09:37 -0300

    Fix function name for hello world n times problem

--- programming_challenges/hacker_rank/hello-world-n-times.clj
+++ programming_challenges/hacker_rank/hello-world-n-times.clj
@@ -1,12 +1,12 @@
 ;; https://www.hackerrank.com/challenges/fp-hello-world-n-times/problem
 
-(defn equal-to-or-less-than-zero?
+(defn greater-than-zero?
   [n]
   (> n 0))
 
 (defn hello-word-n-times
   [n]
-  (if (equal-to-or-less-than-zero? n)
+  (if (greater-than-zero? n)
     (do (println "Hello World")
         (hello-word-n-times (- n 1)))))
 
