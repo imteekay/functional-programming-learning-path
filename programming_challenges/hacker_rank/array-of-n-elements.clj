abbc005477a9cee315c7caf9eb118a5b0b042fae
TK <leandrotk100@gmail.com>
2018-10-11 22:10:38 -0300

    Remove all solutions to add again

--- programming_challenges/hacker_rank/array-of-n-elements.clj
+++
@@ -1,9 +0,0 @@
-;; https://www.hackerrank.com/challenges/fp-array-of-n-elements/problem
-
-(defn array-of-n-elements
-  [n]
-  (range n))
-
-(array-of-n-elements 1)
-(array-of-n-elements 3)
-(array-of-n-elements 5)
--- programming_challenges/hacker_rank/hello-world.clj
+++
@@ -1,3 +0,0 @@
-;; Problem: https://www.hackerrank.com/challenges/fp-hello-world/problem?h_r=next-challenge&h_v=zen
-
-(print "Hello World")
