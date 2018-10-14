1c92036707e1b2d3f86a264b6367143485f7e82b
TK <leandrotk100@gmail.com>
2018-10-13 22:52:38 -0300

    Solve sum of odd elements problem

--- programming_challenges/hacker_rank/sum-of-odd-elements.clj
+++ programming_challenges/hacker_rank/sum-of-odd-elements.clj
@@ -1,11 +1,11 @@
 ;; https://www.hackerrank.com/challenges/fp-sum-of-odd-elements/problem
 
 (defn sum-of-odd-elements
-  [list]
-  (loop [total 0 lsl list]
-    (if (odd? (first list))
-      (println (first list)))
-    (if (not-empty (rest list))
-      (recur (rest list)))))
+  [lst]
+  (reduce
+    +
+    (filter
+      (fn [n] (odd? n))
+      lst)))
 
 (sum-of-odd-elements [3 2 4 6 5 7 8 0 1])
