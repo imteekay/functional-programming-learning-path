727cae0b9267c331e1b464bd257000596b9b91e7
TK <leandrotk100@gmail.com>
2018-10-14 13:40:22 -0300

    Add first exercism: hello world

---
+++ programming_challenges/exercism/hello-world/project.clj
@@ -0,0 +1,4 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "hello-world exercise."
+  :url "https://github.com/exercism/clojure/tree/master/exercises/hello-world"
+  :dependencies [[org.clojure/clojure "1.9.0"]])
