654edf4c2d96bed0ec5277e3f43fb6d65c17dd9a
TK <leandrotk100@gmail.com>
2018-10-16 08:31:59 -0300

    Solve reverse list problem using reduce

--- programming_challenges/hacker_rank/reverse-a-list.clj
+++ programming_challenges/hacker_rank/reverse-a-list.clj
@@ -4,6 +4,8 @@
 ;; take function: returns the first N taken elements of a collection
 ;; not-empty function: returns true if the collection is not empty. Otherwise, false
 
+(def example-list [19 22 3 28 26 17 18 4 28 0])
+
 (defn reverse-a-list
   [list]
   (loop [original-list list reversed-list []]
@@ -11,6 +13,12 @@
       (recur (take (dec (count original-list)) original-list) (conj reversed-list (last original-list)))
       reversed-list)))
 
-(def example-list [19 22 3 28 26 17 18 4 28 0])
-
 (reverse-a-list example-list)
+
+;; Implementing an anonymous function
+
+(fn [lst]
+  (loop [original-list lst reversed-list []]
+    (if (not-empty original-list)
+      (recur (take (dec (count original-list)) original-list) (conj reversed-list (last original-list)))
+      reversed-list)))
