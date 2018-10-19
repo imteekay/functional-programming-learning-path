f4676d75a45724ced91f38184d59dc8293842376
TK <leandrotk100@gmail.com>
2018-10-19 00:07:20 -0300

    Refactor job algorithm

--- programming_challenges/job.clj
+++ programming_challenges/job.clj
@@ -1,14 +1,17 @@
 (def jobs [{:id 1 :urgency false} {:id 2 :urgency true} {:id 3 :urgency false}])
 
-(def map-urgency {true 0 false 1})
+(def urgency-enum {true 0 false 1})
 
 (defn job-urgency-to-int
   [job]
-  (get map-urgency (:urgency job)))
+  (get urgency-enum (:urgency job)))
 
-(sort-by :urgency (reduce
-  (fn
-    [mutated-jobs job]
-    (conj mutated-jobs (assoc job :urgency (job-urgency-to-int job))))
+(defn update-urgency
+  [job]
+  (assoc job :urgency (job-urgency-to-int job)))
+
+(defn new-jobs
   []
-  jobs))
+  (map update-urgency jobs))
+
+(sort-by :urgency (new-jobs))
