357ad7f0789bf603a40bac35bacab768a77647a0
TK <leandrotk100@gmail.com>
2018-10-19 00:21:04 -0300

    Add agents "object" list

--- programming_challenges/job.clj
+++ programming_challenges/job.clj
@@ -1,4 +1,17 @@
-(def jobs [{:id 1 :urgency false} {:id 2 :urgency true} {:id 3 :urgency false}])
+(def jobs
+  [{:id 1 :urgency false}
+   {:id 2 :urgency true}
+   {:id 3 :urgency false}])
+
+(def agents
+  [{:id "8ab86c18-3fae-4804-bfd9-c3d6e8f66260"
+    :name "BoJack Horseman"
+    :primary_skillset ["bills-questions"]
+    :secondary_skillset []}
+   {:id "ed0e23ef-6c2b-430c-9b90-cd4f1ff74c88"
+    :name "Mr. Peanut Butter"
+    :primary_skillset ["rewards-question"]
+    :secondary_skillset ["bills-questions"]}])
 
 (def urgency-enum {true 0 false 1})
 
@@ -14,4 +27,6 @@
   []
   (map update-urgency jobs))
 
-(sort-by :urgency (new-jobs))
+(defn sorted-jobs
+  []
+  (sort-by :urgency (new-jobs)))
