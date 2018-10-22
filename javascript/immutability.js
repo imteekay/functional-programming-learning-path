d0a362b24477e762b4bd816eafcb4e89592de28b
TK <leandrotk100@gmail.com>
2018-10-21 23:37:26 -0300

    Add more info about immutability

--- javascript/immutability.js
+++ javascript/immutability.js
@@ -3,4 +3,12 @@ var name = 'TK';
 name[0] = 'D';
 console.log(name); // 'TK'
 
-/* immutable data is important because we need to be able to attach dependable, unchanging values to our functions */
+/*
+  immutable data is important because we need to be able to attach dependable, unchanging values to our functions
+
+  In multi-threaded applications that is a great thing.
+  It allows for a thread to act on data represented by immutable objects
+  without worrying what other threads are up to.
+
+  immutable objects are more thread-safe than mutable objects.
+*/
