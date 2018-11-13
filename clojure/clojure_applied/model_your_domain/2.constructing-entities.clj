8fbfa91c8db9f03768a503447a261b638ae4c362
TK <leandrotk100@gmail.com>
2018-11-13 00:13:11 -0200

    Apply cljfmt for constructing entities

--- clojure/clojure_applied/model_your_domain/2.constructing-entities.clj
+++ clojure/clojure_applied/model_your_domain/2.constructing-entities.clj
@@ -5,15 +5,6 @@
 (defn make-entity [n1 n2 & [n3 n4]])
 
 ;; implementing a Money value object
-;---
-; Excerpted from "Clojure Applied",
-; published by The Pragmatic Bookshelf.
-; Copyrights apply to this code. It may not be used to create training material,
-; courses, books, articles, and the like. Contact us if you are in doubt.
-; We make no guarantees that this code is fit for any purpose.
-; Visit http://www.pragmaticprogrammer.com/titles/vmclojeco for more book information.
-;---
-;
 (ns ch1.money)
 
 (declare validate-same-currency)
@@ -22,9 +13,9 @@
 
 (defrecord Money [amount ^Currency currency]
   java.lang.Comparable
-    (compareTo [m1 m2]
-      (validate-same-currency m1 m2)
-      (compare (:amount m1) (:amount m2))))
+  (compareTo [m1 m2]
+    (validate-same-currency m1 m2)
+    (compare (:amount m1) (:amount m2))))
 
 (def currencies {:usd (->Currency 100 "USD" "US Dollars")
                  :eur (->Currency 100 "EUR" "Euro")})
@@ -33,22 +24,22 @@
   [m1 m2]
   (or (= (:currency m1) (:currency m2))
       (throw
-        (ex-info "Currencies do not match."
-          {:m1 m1 :m2 m2}))))
+       (ex-info "Currencies do not match."
+                {:m1 m1 :m2 m2}))))
 
 (defn =$
   ([m1] true)
   ([m1 m2] (zero? (.compareTo m1 m2)))
   ([m1 m2 & monies]
-    (every? zero? (map #(.compareTo m1 %) (conj monies m2)))))
+   (every? zero? (map #(.compareTo m1 %) (conj monies m2)))))
 
 (defn +$
   ([m1] m1)
   ([m1 m2]
-    (validate-same-currency m1 m2)
-    (->Money (+ (:amount m1) (:amount m2)) (:currency m1)))
+   (validate-same-currency m1 m2)
+   (->Money (+ (:amount m1) (:amount m2)) (:currency m1)))
   ([m1 m2 & monies]
-    (reduce +$ m1 (conj monies m2))))
+   (reduce +$ m1 (conj monies m2))))
 
 (defn *$ [m n] (->Money (* n (:amount m)) (:currency m)))
 
@@ -57,8 +48,12 @@
   ([amount] (make-money amount :usd))
   ([amount currency] (->Money amount currency)))
 
-(defn -$ [m1 m2 & monies] ,,,)
+(defn -$ [m1 m2 & monies])
 
-(defn allocate [m proportions] ,,,)
+(defn allocate [m proportions])
 
 (def zero-dollars (make-money 0 :usd))
+
+(make-money)
+(make-money 1)
+(make-money 5 (:eur currencies))
