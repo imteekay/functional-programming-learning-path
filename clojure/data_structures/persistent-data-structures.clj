2e0e32f22a663a8bfc478170e1a23b4e937f3039
TK <leandrotk100@gmail.com>
2019-01-03 22:50:03 -0200

    Improvements on the tree implementation

--- clojure/data_structures/persistent-data-structures.clj
+++ clojure/data_structures/persistent-data-structures.clj
@@ -1,39 +1,17 @@
-(def persistent-vector [0 1 2 3 4])
-
-;; print the initialized vector
-persistent-vector
-
-;; return a new persistent vector with 5 added
-(conj persistent-vector 5)
-
-;; see if persistent-vector changed
-persistent-vector
-
-;; comparing persistend-vector with the new vector created
-(= persistent-vector (conj persistent-vector 5)) ;; returns false
-
-; Persistent Lists
-(def baselist (list :barnabas :adam))
-(def lst1 (cons :willie baselist))
-(def lst2 (cons :phoenix baselist))
-
-lst1 ; (:willie :barnabas :adam)
-lst2 ; (:phoenix :barnabas :adam)
-
-(next lst1)
-
-; tree with map
-{:val 5 :L nil :R nil}
+(defn new-tree [val L R]
+  {:val val
+   :L L
+   :R R})
 
 (defn build-tree [tree value]
   (cond
     (nil? tree) {:val value :L nil :R nil}
-    (< value (:val tree)) {:val (:val tree)
-                           :L (build-tree (:L tree) value)
-                           :R (:R tree)}
-    :else {:val (:val tree)
-           :L (:L tree)
-           :R (build-tree (:R tree) value)}))
+    (< value (:val tree)) (new-tree (:val tree)
+                                    (build-tree (:L tree) value)
+                                    (:R tree))
+    :else (new-tree (:val tree)
+                    (:L tree)
+                    (build-tree (:R tree) value))))
 
 (def tree (build-tree nil 5))
 (def tree (build-tree tree 4))
