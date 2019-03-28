358f62e05a5e3d42d6f40159fb1d901e84634166
TK <leandrotk100@gmail.com>
2019-03-28 15:08:26 -0300

    Solve resistor color problem

---
+++ programming_challenges/exercism/javascript/resistor-color.js
@@ -0,0 +1,16 @@
+const colorCode = color => COLORS.indexOf(color);
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
+
+export { colorCode, COLORS };
