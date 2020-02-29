8287430e9e1755f2d35436703e238b31b2ed3e67
TK <leandrotk100@gmail.com>
2020-02-29 20:25:50 -0300

    Add haskell basics

---
+++ haskell/basics.hs
@@ -0,0 +1,9 @@
+head [1, 2, 3, 4, 5] -- get the first: 1
+tail [1, 2, 3, 4, 5] -- get the last: 5
+take 3 [1, 2, 3, 4, 5] -- get the first n elements: [1, 2, 3]
+drop 3 [1, 2, 3, 4, 5] -- remove the first n element: [4, 5]
+length [1, 2, 3, 4, 5] -- 5
+sum [1, 2, 3, 4, 5] -- sum all elements in the list: 15
+product [1, 2, 3, 4, 5] -- product all elements in the list: 120
+[1, 2, 3] ++ [4, 5] -- append two lists: [1, 2, 3, 4, 5]
+reverse [1, 2, 3, 4, 5] -- reverse all elements in the list: [5, 4, 3, 2, 1]
