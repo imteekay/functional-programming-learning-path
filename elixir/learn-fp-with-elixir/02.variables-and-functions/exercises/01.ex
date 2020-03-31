f404974155d0c287df6c8076b176a66eff947e8d
TK <leandrotk100@gmail.com>
2020-03-31 20:32:07 -0300

    solve exercise 1

---
+++ elixir/learn-fp-with-elixir/02.variables-and-functions/exercises/01.ex
@@ -0,0 +1,6 @@
+# Create an expression that solves the following problem:
+# Sarah has bought ten slices of bread for ten cents each,
+# three bottles of milk for two dollars each, and a cake for fifteen dollars.
+# How many dollars has Sarah spent?
+
+10 * 0.10 + 3 * 2 + 15 # 22.0
