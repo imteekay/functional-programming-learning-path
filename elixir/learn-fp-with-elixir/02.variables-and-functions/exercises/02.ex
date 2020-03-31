a421c0298db0a600920b2d006b33a66257e7e158
TK <leandrotk100@gmail.com>
2020-03-31 20:37:42 -0300

    Solve exercise 02

---
+++ elixir/learn-fp-with-elixir/02.variables-and-functions/exercises/02.ex
@@ -0,0 +1,10 @@
+# Bob has traveled 200 km in four hours.
+# Using variables, print a message showing his travel distance, time, and average velocity.
+
+distance = 200
+time = 4
+average_velocity = 200 / 4
+
+IO.inspect distance
+IO.inspect time
+IO.inspect average_velocity
