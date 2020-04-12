866624b2336d32b10e6c5cf777b76d238cc74544
TK <leandrotk100@gmail.com>
2020-04-12 15:58:42 -0300

    control flow with pattern matching

---
+++ elixir/learn-fp-with-elixir/03.using-pattern-matching-to-control-the-program-flow/01.equal_operator.ex
@@ -0,0 +1,5 @@
+1 = 1 # ok
+1 = 2 # raises an error
+x = 1 # ok
+1 = x # ok
+2 = x # raises an error
---
+++ elixir/learn-fp-with-elixir/03.using-pattern-matching-to-control-the-program-flow/02.destructuring.ex
@@ -0,0 +1,9 @@
+# strings
+"Authentication: " <> credentials = "Authentication: Basic dXNlcjpwYXNz"
+IO.inspect credentials
+
+# tuples
+{a, b, c} = {1, 2, 3}
+IO.inspect a
+IO.inspect b
+IO.inspect c
