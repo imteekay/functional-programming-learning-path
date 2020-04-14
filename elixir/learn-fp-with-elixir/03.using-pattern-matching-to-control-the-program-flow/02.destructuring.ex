16b0d8048396fd36c8265ae613117010bfa3784f
TK <leandrotk100@gmail.com>
2020-04-13 21:46:22 -0300

    Handling lists

--- elixir/learn-fp-with-elixir/03.using-pattern-matching-to-control-the-program-flow/02.destructuring.ex
+++ elixir/learn-fp-with-elixir/03.using-pattern-matching-to-control-the-program-flow/02.destructuring.ex
@@ -7,3 +7,25 @@ IO.inspect credentials
 IO.inspect a
 IO.inspect b
 IO.inspect c
+
+# lists
+[a, b, c] = [1, 2, 3]
+IO.inspect a
+IO.inspect b
+IO.inspect c
+
+[_, x, _] = [1, 2, 3] # this _ operator is called wild card
+IO.inspect x
+
+[head | rest] = [1, 2, 3, 4, 5]
+IO.inspect head # 1
+IO.inspect rest # [2, 3, 4, 5]
+
+[first, second | rest] = [1, 2, 3, 4, 5]
+IO.inspect first # 1
+IO.inspect second # 2
+IO.inspect rest # [2, 3, 4, 5]
+
+[head | rest] = [:a]
+IO.inspect head # :a
+IO.inspect rest # []
