803e0d9416a0319d62bebeb86e1ba57cf6822680
TK <leandrotk100@gmail.com>
2020-05-10 16:23:30 -0300

    destructuring maps

--- elixir/learn-fp-with-elixir/03.using-pattern-matching-to-control-the-program-flow/02.destructuring.ex
+++ elixir/learn-fp-with-elixir/03.using-pattern-matching-to-control-the-program-flow/02.destructuring.ex
@@ -29,3 +29,15 @@ IO.inspect rest # [2, 3, 4, 5]
 [head | rest] = [:a]
 IO.inspect head # :a
 IO.inspect rest # []
+
+# maps
+user_signup = %{username: "TK", email: "tk@mail.com", password: "123123123"}
+IO.inspect user_signup
+
+user_signup = %{:username => "TK", :email => "tk@mail.com", :password => "123123123"} # another map syntax
+
+%{email: email} = user_signup
+IO.inspect email
+
+%{username: username = "TK"} = user_signup
+IO.inspect username
